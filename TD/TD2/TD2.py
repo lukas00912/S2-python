94ab60f85627cf655ad2d21e9c17d4c6bcd1ad36
Lukas Chatrieux <lukaschatrieux1@outlook.fr>
2024-03-04 15:39:05 +0100

    //

--- TD/TD2/TD2.py
+++ TD/TD2/TD2.py
@@ -4,25 +4,28 @@ fenetre = tk.Tk()
 fenetre.geometry('600x600') 
 fenetre.title("Mon dessin")
 
+def couleur():
+    couleur = input("Donner une couleur")
+
 def fermer_fenetre() :
     fenetre.destroy()
 
 def cercle():
-    canvas.create_oval(100,100,10,10, width=2, fill="blue", outline="blue")
+    canvas.create_oval(100,100,10,10, width=2, fill="blue", outline=COULEUR)
 
 def carre():
-    canvas.create_rectangle(100, 100, 30, 30, fill="red", outline="red")
+    canvas.create_rectangle(100, 100, 30, 30, fill="red", outline=COULEUR)
 
 def croix():
-    canvas.create_line((150,160),(60,50), fill="yellow")
-    canvas.create_line((50,60),(160,150), fill="yellow")
+    canvas.create_line((150,160),(60,50), fill=COULEUR)
+    canvas.create_line((50,60),(160,150), fill=COULEUR)
     
 canvas = tk.Canvas(width=300, height=300, bg="black")
 bouton_carre = tk.Button(fenetre, text="Carré", command=carre)
 bouton_cercle = tk.Button(fenetre, text="Cercle", command=cercle)
 bouton_croix = tk.Button(fenetre, text="Croix", command=croix)    
 bouton_quitter = tk.Button(fenetre, text="Fermer la fenêtre", command=fermer_fenetre)
-bouton_couleurs = tk.Button(fenetre, text="Choisir une couleur")
+bouton_couleurs = tk.Button(fenetre, text="Choisir une couleur", command=couleur)
 
 canvas.grid(row=2, column=1, rowspan=4,  columnspan=1)
 bouton_carre.grid(row=3,column=0)
