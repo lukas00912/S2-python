b42cb815de8b81bd7e6351bff70bd92e68fc0486
Lukas Chatrieux <lukaschatrieux1@outlook.fr>
2024-03-25 14:19:10 +0100

    modif

--- TD/TD2/cible.py
+++ TD/TD2/cible.py
@@ -1,15 +1,16 @@
 import tkinter as tk
 
 fenetre = tk.Tk()
-fenetre.geometry('600x600') 
+fenetre.geometry("600x600")
 fenetre.title("Mon dessin")
 
-def fermer_fenetre() :
+
+def fermer_fenetre():
     fenetre.destroy()
 
-def cercle():
-    canvas.create_oval(1000,1000,50,50, width=2, outline="blue")
 
+def cercle():
+    canvas.create_oval(1000, 1000, 50, 50, width=2, outline="blue")
 
 
 canvas = tk.Canvas(width=300, height=300, bg="black")
@@ -17,10 +18,9 @@ bouton_quitter = tk.Button(fenetre, text="Fermer la fenêtre", command=fermer_fe
 bouton_cercle = tk.Button(fenetre, text="Cercle", command=cercle)
 
 
-canvas.grid(row=2, column=1, rowspan=4,  columnspan=1)
+canvas.grid(row=2, column=1, rowspan=4, columnspan=1)
 bouton_quitter.grid(row=5, column=0)
-bouton_cercle.grid(row=2,column=0)
-
+bouton_cercle.grid(row=2, column=0)
 
 
 fenetre.mainloop()
